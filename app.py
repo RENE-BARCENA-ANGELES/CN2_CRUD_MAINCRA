e16fc1898e80bc52e260d046da533bcbb2d057e9
stripmau <l22590373@sjuanrio.tecnm.mx>
2026-09-22 18:48:09 -0600

    eliminar

--- app.py
+++ app.py
@@ -60,5 +60,16 @@ def add_post():
     return render_template('create_post.html', categories=categories)
 
 
+
+#Eliminar post
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post = Post.query.get(id)
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
